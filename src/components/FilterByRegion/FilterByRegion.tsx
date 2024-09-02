aa04e57d76f5ce4f534dc647fbc976e09841b515
Stoychev <stoychev.nas@gmail.com>
2024-09-02 08:15:06 +0300

    Changes created with add hover effect on mouse over on filter button and close dropdown filter when use click outside of filter field

--- src/components/FilterByRegion/FilterByRegion.tsx
+++ src/components/FilterByRegion/FilterByRegion.tsx
@@ -1,42 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import { RiArrowDropDownLine } from "react-icons/ri";
 import styles from './FilterByRegion.module.css';
 
 interface DropdownProps {
-  changeShow: Function
-  dark: boolean
+    changeShow: Function
+    dark: boolean
 }
 
 const FILTER_STRING = 'Filter by Region'
 
 export default function FilterByRegion({ changeShow, dark }: DropdownProps) {
-  const [selectedOption, setSelectedOption] = useState<string>(FILTER_STRING);
+    const [selectedOption, setSelectedOption] = useState<string>(FILTER_STRING);
 
-  const handleSelectChange = (value: string) => {
-    const selectedValue = value;
-    setSelectedOption(selectedValue);
-    changeShow('region', selectedValue);
-  };
+    const dropdownRef = useRef<HTMLDivElement>(null);
 
-  const [isOpen, setIsOpen] = useState(false);
-  const menuItems = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+    const handleSelectChange = (value: string) => {
+        const selectedValue = value;
+        setSelectedOption(selectedValue);
+        changeShow('region', selectedValue);
+    };
 
-  return (
-    <div className={styles.dropdown}>
-      <div className={styles.dropdownWrapper}>
-        <button onClick={() => setIsOpen(!isOpen)} className={dark ? styles.selectDark : styles.selectLight}>
-          {selectedOption}
-          <RiArrowDropDownLine />
-        </button>
-      </div>
-      {isOpen && (
-        <div className={dark ? styles.optionsWrapperDark : styles.optionsWrapperLight}>
-          {menuItems.map((item, index) => (
-            <button key={index} className={styles.buttonOptions} onClick={() => handleSelectChange(item)}>{item}</button>
-          ))}
+    const [isOpen, setIsOpen] = useState(false);
+    const menuItems = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+
+    const handleClickOutside = (event: MouseEvent) => {
+        const target = event.target as Node;
+
+        if (dropdownRef.current && !dropdownRef.current.contains(target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div className={styles.dropdown} ref={dropdownRef}>
+            <div className={styles.dropdownWrapper}>
+                <button onClick={() => setIsOpen(!isOpen)} className={dark ? styles.selectDark : styles.selectLight}>
+                    {selectedOption}
+                    <RiArrowDropDownLine />
+                </button>
+            </div>
+            {isOpen && (
+                <div className={dark ? styles.optionsWrapperDark : styles.optionsWrapperLight}>
+                    {menuItems.map((item, index) => (
+                        <button key={index} className={styles.buttonOptions} onClick={() => handleSelectChange(item)}>{item}</button>
+                    ))}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  )
+    )
 };
